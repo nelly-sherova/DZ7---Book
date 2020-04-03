1eb6376b3e8e61c56ce2048e649b2ce44bb14fa2
unknown <lvova.nikol@mail.ru>
2020-04-03 15:23:25 +0300

    Объявление класса

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bookk
+{
+    class Book
+    {
+        Title title = new Title();
+        Author author = new Author();
+        Content content = new Content();
+        public Book(string title, string author, string content)
+        {
+            this.title.title = title;
+            this.author.author = author;
+            this.content.content = content;
+        }
+    }
+    class Title
+    {
+        private string title { get; set; }
+    }
+    class Author
+    {
+        private string author { get; set; }
+    }
+    class Content 
+    {
+        private string content { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
